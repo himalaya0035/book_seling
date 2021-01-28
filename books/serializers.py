cf81aa0f6a0468c18188bb5b829e6c5c0751c10c
killewolf <singhpriyansh51001@gmail.com>
2021-01-28 11:34:05 +0530

    updated book serializer

--- books/serializers.py
+++ books/serializers.py
@@ -3,7 +3,6 @@ from rest_framework import serializers
 from .models import Book, Genre, Author
 
 
-# todo
 class AuthorSerializer(serializers.ModelSerializer):
     created_by = serializers.HiddenField(default=serializers.CurrentUserDefault())
 
@@ -31,30 +30,30 @@ class BookSerializer(serializers.ModelSerializer):
 
     def __init__(self, *args, **kwargs):
         remove_fields = kwargs.pop('remove_fields', None)
+        insertion = kwargs.pop('insert', False)
         super(BookSerializer, self).__init__(*args, **kwargs)
         if remove_fields:
             for remove_field in remove_fields:
                 self.fields.pop(remove_field)
 
+        if not insertion:
+            try:
+                self.fields.pop('genre')
+                self.fields.pop('author')
+            except KeyError as _:
+                pass
+
+    created_by = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
     author_details = serializers.SerializerMethodField()
     genre_names = serializers.SerializerMethodField()
-
     lowest_price = serializers.SerializerMethodField('get_lowest_price')
 
     class Meta:
         model = Book
         fields = ['ISBN', 'name', 'genre_names', 'author_details', 'genre', 'author', 'cover_image',
-                  'released_date', 'info', 'rating', 'lowest_price']
+                  'released_date', 'info', 'rating', 'lowest_price', 'created_by']
         read_only_fields = ['genre_names', 'author_details', 'lowest_price']
-
-    def to_representation(self, instance):
-        ret = super().to_representation(instance)
-        try:
-            del ret['genre']
-            del ret['author']
-        except KeyError as _:
-            pass
-        return ret
 
     def get_lowest_price(self, instance: Book, *args, **kwargs):
         lowest_price = instance.all_deals.filter(quantity__gt=0).values('price', 'id', 'quantity').order_by(
--- books/views.py
+++ books/views.py
@@ -1,5 +1,7 @@
 from django.core.cache import cache
 from django.db.models import Count
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, status
 from rest_framework.generics import RetrieveUpdateDestroyAPIView, ListCreateAPIView, ListAPIView, RetrieveAPIView
@@ -13,7 +15,8 @@ from .permissions import UpdateDeleteObjectPermission
 from .serializers import AuthorSerializer, BookSerializer, GenreSerializer, BookIconSerializer
 
 
-# name__exact
+# todo caching
+
 class BookListView(ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookListSerializer
@@ -22,15 +25,6 @@ class BookListView(ListAPIView):
     filterset_fields = ['name', 'author__name', 'genre__name', 'ISBN',
                         'author__id']
     search_fields = ['name', 'author__name', 'genre__name']
-
-    def get_serializer_context(self):
-        context = super(BookListView, self).get_serializer_context()
-        context.update({'request': self.request})
-        return context
-
-    # @method_decorator(cache_page(60 * 5))
-    # def dispatch(self, *args, **kwargs):
-    #     return super(BookListView, self).dispatch(*args, **kwargs)
 
 
 class BookActionView(RetrieveUpdateDestroyAPIView):
@@ -61,6 +55,10 @@ class GenreRetrieveView(RetrieveAPIView):
     serializer_class = GenreSerializer
     lookup_field = 'name'
 
+    @method_decorator(cache_page(60 * 15))
+    def dispatch(self, request, *args, **kwargs):
+        super(GenreRetrieveView, self).dispatch(request, *args, **kwargs)
+
 
 class GenreListView(ListAPIView):
     filter_backends = [DjangoFilterBackend, filters.SearchFilter]
@@ -75,7 +73,6 @@ class GetRecommendedBooks(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
-        print('here')
         user = request.user
         data = cache.get(f'{user.id}')
 
@@ -87,8 +84,8 @@ class GetRecommendedBooks(APIView):
             data = Book.objects.filter(genre__in=fav_genres).order_by('-sold_quantity').order_by('-rating')[:9]
             data = BookIconSerializer(data, many=True).data
 
-            cache.set(f'{user.id}', data, 60 * 60)
-            # cache timeout=1hr
+            cache.set(f'{user.id}', data, 60 * 20)
+            # cache timeout=20 min
 
         return Response(data=data, status=status.HTTP_200_OK)
 
@@ -98,16 +95,11 @@ class BestSellerView(ListAPIView):
     queryset = Book.objects.order_by('-date_created').order_by('-rating')[:9]
 
 
-# todo
 class TopAuthors(ListAPIView):
-    # serializer_class = AuthorSerializer
     queryset = Author.objects.annotate(booksWritten=Count('book')).order_by('-booksWritten')[:9]
 
-    # Author.objects.order_by('book__rating')
-    def list(self, request, *args, **kwargs):
-        queryset = self.get_queryset()
-        data = AuthorSerializer(queryset, many=True, remove_fields=['info']).data
-        return Response(data)
+    def get_serializer(self, *args, **kwargs):
+        return AuthorSerializer(*args, **kwargs, remove_fields=['info'])
 
 
 # class RegisterNewBook(APIView):
